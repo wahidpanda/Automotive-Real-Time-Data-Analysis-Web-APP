0d32ea05426cd0f705c62f9dace19295319f41c3
Wahid <islamoahidul12@gmail.com>
2023-10-16 20:30:56 +0600

    Update 1_🤓_Homepage.py

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -2,10 +2,10 @@ import streamlit as st
 import time
 from auth import authenticate
 import random
-
+import duckdb
 import pandas as pd
-
-
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Set page title and icon
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
