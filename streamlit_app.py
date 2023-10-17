5fec4e2d807873eb287b4eeb4e61d9fd39d9879e
Wahid <islamoahidul12@gmail.com>
2023-10-17 11:45:20 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def login():
     # Embed Google Calendar on the right side of the page
     st.markdown(
         """
-    <iframe src="https://calendar.google.com/calendar/embed?height=300&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FDhaka&src=your_email%40domain.com&color=%23853104&showTz=0&showCalendars=0"
+    <iframe src="https://calendar.google.com/calendar/embed?height=300&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FDhaka&src=your_email%40domain.com&color=%128282showTz=0&showCalendars=0"
         width="100" height="100" frameborder="0" scrolling="no" style="float: right;"></iframe>
     """,
         unsafe_allow_html=True,
